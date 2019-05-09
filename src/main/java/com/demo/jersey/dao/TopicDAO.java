5cceff99e4e3be7e669b17bd9e1471bc52048ae3
alessanderleite <alessander.logistica@gmail.com>
2019-05-09 17:07:56 -0300

    code the TopicDAO interface

--- src/main/java/com/demo/jersey/dao/TopicDAO.java
+++ src/main/java/com/demo/jersey/dao/TopicDAO.java
@@ -1,5 +1,14 @@
 package com.demo.jersey.dao;
 
-public interface TopicDAO {
+import java.util.List;
 
+import com.demo.jersey.entities.Topic;
+
+public interface TopicDAO {
+	public abstract List<Topic> getAllTopics();
+	public abstract Topic getTopicById(int topicId);
+	public abstract void addTopic(Topic topic);
+	public abstract void updateTopic(Topic topic);
+	public abstract void deleteTopic(int topicId);
+	public abstract boolean topicExists(String title, String category);
 }
