e99996de7a059f97cea01b8678c87ecd4a7b9a4b
alessanderleite <alessander.logistica@gmail.com>
2019-05-09 15:57:53 -0300

    code the class Topic, and add the Entity and Table annotations

--- src/main/java/com/demo/jersey/entities/Topic.java
+++ src/main/java/com/demo/jersey/entities/Topic.java
@@ -1,5 +1,53 @@
 package com.demo.jersey.entities;
 
-public class Topic {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_topics")
+public class Topic implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int topicId;
+	
+	@Column(name = "title", nullable = false, length = 100)
+	private String title;
+	
+	@Column(name = "category", nullable = false, length = 200)
+	private String category;
+
+	public int getTopicId() {
+		return topicId;
+	}
+
+	public void setTopicId(int topicId) {
+		this.topicId = topicId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
 }
