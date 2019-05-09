b041f0754358f20c869bba6ed5ab7599f54a0de2
alessanderleite <alessander.logistica@gmail.com>
2019-05-09 18:01:51 -0300

    implements TopicService

--- src/main/java/com/demo/jersey/service/TopicServiceImpl.java
+++ src/main/java/com/demo/jersey/service/TopicServiceImpl.java
@@ -1,5 +1,48 @@
 package com.demo.jersey.service;
 
-public class TopicServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.jersey.dao.TopicDAO;
+import com.demo.jersey.entities.Topic;
+
+@Service
+public class TopicServiceImpl implements TopicService {
+	
+	@Autowired
+	private TopicDAO topicDAO;
+
+	@Override
+	public List<Topic> getAllTopics() {
+		return topicDAO.getAllTopics();
+	}
+
+	@Override
+	public Topic getTopicById(int topicId) {
+		Topic topic = topicDAO.getTopicById(topicId);
+		return topic;
+	}
+
+	@Override
+	public synchronized boolean addTopic(Topic topic) {
+		if (topicDAO.topicExists(topic.getTitle(), topic.getCategory())) {
+			return false;
+		} else {
+			topicDAO.addTopic(topic);
+			return true;	
+		}
+	}
+
+	@Override
+	public void updateTopic(Topic topic) {
+		topicDAO.updateTopic(topic);
+	}
+
+	@Override
+	public void deleteTopic(int topicId) {
+		topicDAO.deleteTopic(topicId);
+	}
 
 }
