2d6876f242aa4173434a2bec130517525390d659
alessanderleite <alessander.logistica@gmail.com>
2019-05-09 15:34:28 -0300

    initial commit

---
+++ src/main/java/com/demo/jersey/JaxRsJerseyWebServiceMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxRsJerseyWebServiceMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxRsJerseyWebServiceMySqlApplication.class, args);
+	}
+
+}
