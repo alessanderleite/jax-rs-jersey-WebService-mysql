87d3e5bcbc103ae510d52bca428063ac1b1d7338
alessanderleite <alessander.logistica@gmail.com>
2019-05-09 17:23:29 -0300

    code the TopicService interface

--- src/main/java/com/demo/jersey/service/TopicService.java
+++ src/main/java/com/demo/jersey/service/TopicService.java
@@ -1,5 +1,13 @@
 package com.demo.jersey.service;
 
-public interface TopicService {
+import java.util.List;
 
+import com.demo.jersey.entities.Topic;
+
+public interface TopicService {
+	public abstract List<Topic> getAllTopics();
+	public abstract Topic getTopicById(int topicId);
+	public abstract boolean addTopic(Topic topic);
+	public abstract void updateTopic(Topic topic);
+	public abstract void deleteTopic(int topicId);
 }
